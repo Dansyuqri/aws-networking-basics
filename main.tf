5da9fbb49cd1a4bf9ba91ae89038157053a924b1
Syuqri <shooks@Syuqris-MacBook-Pro.local>
2023-12-10 15:01:38 +0800

    Add first EC2 instance

--- main.tf
+++ main.tf
@@ -37,3 +37,31 @@ resource "aws_subnet" "second" {
     Name = "second_subnet"
   }
 }
+
+resource "aws_security_group" "first_instance_sg" {
+  name        = "first_instance_sg"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "first_instance_sg"
+  }
+}
+
+resource "aws_instance" "first" {
+  ami           = data.aws_ami.aml2.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.first.id
+  vpc_security_group_ids = [aws_security_group.first_instance_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+
+  tags = {
+    Name = "first_instance"
+  }
+}
